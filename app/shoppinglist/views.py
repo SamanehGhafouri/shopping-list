60fd7a16c29b2db8d6543f7d45b83c81db55dd42
SamanehGhafouri <samaneh2010gh@gmail.com>
2023-07-18 21:09:24 -0400

    Added shoppinglist app, created signup page and redirected signed up user to current page

---
+++ app/shoppinglist/views.py
@@ -0,0 +1,78 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, authenticate
+from django.db import IntegrityError
+from django import forms
+import requests
+
+
+class CustomUserCreationForm(UserCreationForm):
+
+    first_name = forms.CharField(
+        label='First Name',
+        min_length=5, max_length=150)
+    last_name = forms.CharField(
+        label='Last Name', min_length=5,
+        max_length=150)
+
+
+def signupuser(request):
+
+    if request.method == 'GET':
+        return render(
+            request, 'shoppinglist/signupuser.html',
+            {'form': CustomUserCreationForm})
+    else:
+        username = request.POST['username']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+
+        if password1 == password2:
+            body = {
+                'email': username,
+                'password': password2,
+                'first_name': first_name,
+                'last_name': last_name
+            }
+            try:
+                requests.post(
+                    'http://127.0.0.1:8000/api/user/create/',
+                    headers={'Content-Type': 'application/json'},
+                    json=body)
+
+                user = authenticate(
+                    request,
+                    email=username,
+                    password=password2)
+
+                if user is not None:
+                    login(request, user)
+                    return redirect('currentshopinglist')
+            except IntegrityError:
+                return render(
+                    request, 'shoppinglist/signupuser.html',
+                    {
+                        'form': CustomUserCreationForm,
+                        'error': 'The Username already exist'
+                        }
+                    )
+
+        else:
+            "Tell user the password didn't match"
+            print("hello")
+            return render(
+                request,
+                'shoppinglist/signupuser.html',
+                {
+                    'form': CustomUserCreationForm,
+                    'error': 'Passwords did not match'
+                    }
+                )
+
+
+def currentshopinglist(request):
+    return render(
+        request,
+        'shoppinglist/currentshopinglist.html')
